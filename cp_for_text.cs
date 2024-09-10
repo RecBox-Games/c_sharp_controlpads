d7e4c0c8d37637ad5ba8ef47edfa03c0b32f6260
Weston <westonkelliher@gmail.com>
2024-09-10 15:47:14 -0700

    cp_for_text.cs -> public domain

--- cp_for_text.cs
+++ cp_for_text.cs
@@ -1,19 +1,11 @@
-// Copyright 2022-2024 RecBox, Inc.
+// The code in this file is licensed under CC0 1.0 Universal (CC0 1.0)
+// Public Domain Dedication.
+// To view a copy of this license, visit
+// http://creativecommons.org/publicdomain/zero/1.0/
 //
-// This file is part of the c_sharp_controlpads repository.
-//
-// c_sharp_controlpads is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by the 
-// Free Software Foundation, either version 3 of the License, or (at your option)
-// any later version.
-// 
-// c_sharp_controlpads is distributed in the hope that it will be useful, but
-// WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
-// or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
-// more details.
-// 
-// You should have received a copy of the GNU General Public License along with
-// c_sharp_controlpads. If not, see <https://www.gnu.org/licenses/>.
+// You can copy, modify, distribute and perform the work, even for commercial
+// purposes, all without asking permission. See the CC0 Public Domain
+// Dedication for more details.
 
 using System.Collections;
 using System.Collections.Generic;
